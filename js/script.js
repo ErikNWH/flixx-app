9449f35d0db0ef1ffb9a1a1c7be12244c77a2b29
Erik.NWH <erik.nwh@gmail.com>
2025-10-03 22:12:15 +0200

    Added TV shows Details

--- js/script.js
+++ js/script.js
@@ -63,8 +63,8 @@ async function displayMovieDetails() {
 
   const movie = await fetchAPIData(`movie/${movieId}`);
 
-  // Overlay for background image
-  displaBackgroundImage('movie', movie.backdrop_path);
+  // Overlay for background image movies
+  displayBackgroundImage('movie', movie.backdrop_path);
 
   const div = document.createElement('div');
 
@@ -107,8 +107,63 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 }
 
+// Display Show details
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+      ${
+        show.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />`
+          : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+      }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">First Air Date: ${show.first_air_date}</p>
+      <p>${show.overview}</p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      ${show.homepage ? `<a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>` : ''}
+    </div>
+  </div>
+
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Seasons:</span> ${show.number_of_seasons}</li>
+      <li><span class="text-secondary">Episodes:</span> ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Episode Runtime:</span> ${show.episode_run_time.length ? show.episode_run_time[0] : 'N/A'} minutes</li>
+      <li><span class="text-secondary">Last air date</span> ${show.last_air_date}</li>
+      <li><span class="text-secondary">Latest episode</span> ${show.last_episode_to_air.name}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+      ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+    </div>
+  </div>
+`;
+
+  document.querySelector('#show-details').appendChild(div);
+}
 // Display backdrop on Details Pages
-function displaBackgroundImage(type, backgroundPath) {
+function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement('div');
   overlayDiv.style.backgroundImage = `url(http://image.tmdb.org/t/p/original/${backgroundPath})`;
   overlayDiv.style.backgroundSize = 'cover';
@@ -178,8 +233,8 @@ function init() {
     case '/movie-details.html':
       displayMovieDetails();
       break;
-    case '/tv-detaitls.html':
-      console.log('TV Details');
+    case '/tv-details.html':
+      displayShowDetails();
       break;
     case '/search.html':
       console.log('Search');
